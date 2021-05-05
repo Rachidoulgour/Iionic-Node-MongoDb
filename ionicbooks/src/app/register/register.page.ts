016816999f65312d04ae30db0b6bc7004e18c4e9
rachid <olgor@hotmail.com>
2021-05-05 21:26:23 +0200

    register added

--- ionicbooks/src/app/register/register.page.ts
+++ ionicbooks/src/app/register/register.page.ts
@@ -1,15 +1,81 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { ErrorStateMatcher } from '@angular/material/core';
 
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  matcher = new MyErrorStateMatcher();
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      username: new FormControl(null, 
+        Validators.compose([Validators.minLength(8), Validators.maxLength(20)],
+      )),
+      email: new FormControl(null, 
+        [
+          Validators.email,
+          Validators.required
+        ]
+      ),
+      password: new FormControl(null,
+        Validators.compose([ 
+          Validators.required,
+          Validators.pattern(/^(?=\D*\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,30}$/)
+        ]
+        
+      )),
+      confirmPassword: new FormControl(null, 
+         
+          Validators.required
+          
+        
+      ),
+      conditions: new FormControl(false, 
+        
+          Validators.required
+        
+      )
+    }, { validators: this.checkPasswords })
+  }
+
+  checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+    let pass = group.controls.password.value;
+    let confirmPass = group.controls.confirmPassword.value;
+
+    return pass === confirmPass ? null : { notSame: true }
+  }
+
+  signUp() {
+    if (this.form.status === "VALID") {
+      this.authService.signUp(this.form.value).subscribe(
+        res => {
+          localStorage.setItem('token', res['token']);
+          localStorage.setItem('userId', JSON.stringify(res['user_id']))
+          // this.router.navigate(['/validate'])
+          // this.message = "success"
+        },
+        err => {
+          console.log(err)
+        }
+      )
+    }
   }
 
 }
--- ionicbooks/src/app/services/auth.service.ts
+++ ionicbooks/src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private URL ="http://localhost:5050"
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+
+  signUp(user): Observable<any>{
+    console.log(user)
+    return this.http.post<any>(this.URL + '/signup', user)
+  }
 }
